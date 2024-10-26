5d2921caa81cd010a99146ab352c3799b92ace70
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-26 14:32:03 +0100

    Updated functionality of aim trainer

--- AimTrainer.py
+++ AimTrainer.py
@@ -3,43 +3,93 @@ import random
 
 from Target import Target
 
+pygame.init()
+
 # Colours
 ORANGE = (255, 134, 0)
+PURPLE = (121, 48, 136)
+DARK_PURPLE = (80, 32, 90)
 
-pygame.init()
+# Fonts
+TITLE_FONT = pygame.font.SysFont('applesdgothicneo', 60, True)
+SUBTITLE_FONT = pygame.font.SysFont('applesdgothicneo', 40, True)
 
 
 class AimTrainer():
-    def __init__(self, surface, WIDTH=1000, HEIGHT=800):
+    def __init__(self, surface:pygame.surface, startTargets:int=20, WIDTH:int=1000, HEIGHT:int=800):
         # Initialise Default Perimeters for the PyGame Window
         self.WIN = surface
         self.running = True
         self.__WIDTH = WIDTH
         self.__HEIGHT = HEIGHT
 
-        self.currentTarget = Target((100, 100))
+        # Initialise Starting Text
+        self.__startingTitle = TITLE_FONT.render('Aim Trainer', True, PURPLE)
+        self.__startingSubTitle = SUBTITLE_FONT.render(f'Click {startTargets} Targets as fast as you can', True, PURPLE)
+
+        # Initialise Targets
+        self.currentTarget = Target((self.__WIDTH/2, self.__HEIGHT/2))
+
+        self.__startingTargets = startTargets
+        self.__targetsRemaining = startTargets
+        self.__times = []
 
 
-    def draw_window(self):
+    def draw_window(self) -> None:
         'Draws the UI to the screen'
         
         self.WIN.fill(ORANGE)
 
+        # Drawing the target
         self.currentTarget.draw(self.WIN)
+
+        # Drawing the remaining text
+        if self.__targetsRemaining > 0:
+            remainingText = SUBTITLE_FONT.render(f'Remaining: {self.__targetsRemaining}', True, PURPLE)
+            remainingTextRect = remainingText.get_rect(midtop = (self.__WIDTH/2, 10))
+            self.WIN.blit(remainingText, remainingTextRect)
+
+        # Drawing the start of game text
+        if self.__targetsRemaining == self.__startingTargets:
+            titleTextRect = self.__startingTitle.get_rect(center = (self.__WIDTH/2, (self.__HEIGHT/2)-60))
+            subtitleTextRect = self.__startingSubTitle.get_rect(center = (self.__WIDTH/2, (self.__HEIGHT/2)+60))
+            self.WIN.blit(self.__startingTitle, titleTextRect)
+            self.WIN.blit(self.__startingSubTitle, subtitleTextRect)
+
+        # Drawing the end of game information
+        if self.__targetsRemaining == 0:
+            endText = SUBTITLE_FONT.render('Average time per target:', True, PURPLE)
+            endTextRect = endText.get_rect(center = (self.__WIDTH/2, self.__HEIGHT/2-60))
+            endTime = TITLE_FONT.render(f'{self.get_average_time()*1000:.1f}ms', True, DARK_PURPLE)
+            endTimeRect = endTime.get_rect(center = (self.__WIDTH/2, self.__HEIGHT/2))
+
+            self.WIN.blit(endText, endTextRect)
+            self.WIN.blit(endTime, endTimeRect)
         
         pygame.display.update()
+    
+
+    def get_average_time(self) -> float:
+        'Returns the average time to click each target'
+
+        return sum(self.__times[1:]) / len(self.__times)
 
 
-    def generate_target(self):
+    def generate_target(self) -> None:
         'Generates a new target'
 
-        x = random.randint(self.currentTarget.image.get_width()//2, self.__WIDTH - self.currentTarget.image.get_width())
-        y = random.randint(0, self.__HEIGHT - self.currentTarget.image.get_height())
+        if self.__targetsRemaining > 0:
+            self.__times.append(self.currentTarget.get_end_time())
+            x = random.randint(self.currentTarget.image.get_width()//2, self.__WIDTH - self.currentTarget.image.get_width()//2)
+            y = random.randint(self.currentTarget.image.get_height()//2, self.__HEIGHT - self.currentTarget.image.get_height()//2)
 
-        self.currentTarget = Target((x, y))
+            self.currentTarget = Target((x, y))
+
+        else:
+            self.currentTarget = Target((self.__WIDTH/2, self.__HEIGHT/2+150), True)
 
 
-    def app_tick(self):
+    def app_tick(self) -> None:
         'Handles all of the app logic on each tick'
 
         for event in pygame.event.get():
@@ -56,20 +106,20 @@ class AimTrainer():
                     pygame.quit()
                     exit()
 
-        if self.currentTarget.is_clicked():
+        if self.currentTarget.is_clicked() and self.__targetsRemaining > 0:
+            self.__targetsRemaining -= 1
             self.generate_target()
 
         self.draw_window()
 
 
 while __name__ == "__main__":
-    # Initialises the constants and creates pygame window
     FPS = 120
     WIDTH, HEIGHT = 1000, 800
     pygame.display.set_caption("AimTrainer")
     WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    currentApp = AimTrainer(WIN, WIDTH, HEIGHT)
+    currentApp = AimTrainer(WIN, 20, WIDTH, HEIGHT)
 
     while currentApp.running:
         currentApp.app_tick()
--- Target.py
+++ Target.py
@@ -1,33 +1,45 @@
 import pygame
+import time
+
 
 class Target:
-    def __init__(self, coordinates: tuple):
+    def __init__(self, coordinates: tuple, scale:bool=False) -> None:
         'Initialises the textures as coordinates for the button'
         
-        self.image = pygame.image.load('Assets/target.png')
-
+        # Initialises image and location
         self.coordinates = coordinates
-        self.target_rect = self.image.get_rect(topleft=self.coordinates)
-        self.clicked = False
-
-
-    def is_clicked(self):
-        'Returns True if the button is clicked, and False if button is not clicked'
-
-        mouse_pos = pygame.mouse.get_pos()
+        if scale:
+            self.image = pygame.transform.scale2x(pygame.image.load('Assets/target.png'))
+        else:
+            self.image = pygame.image.load('Assets/target.png')
+        self.targetRect = self.image.get_rect(center=self.coordinates)
         
-        if pygame.mouse.get_pressed()[0] and self.target_rect.collidepoint(mouse_pos):
-            self.clicked = True
+        # Initialises local variables
+        self.__clicked = False
+        self.__startTime = time.perf_counter()
 
-        elif self.clicked:
+
+    def get_end_time(self) -> float:
+        return time.perf_counter() - self.__startTime
+
+
+    def is_clicked(self) -> bool:
+        'Returns True if the target is clicked, and False if button is not clicked'
+
+        mousePos = pygame.mouse.get_pos()
+        
+        if pygame.mouse.get_pressed()[0] and self.targetRect.collidepoint(mousePos):
+            self.__clicked = True
+
+        elif self.__clicked:
             return True
 
         else:
-            self.clicked = False
+            self.__clicked = False
 
 
-    def draw(self, surface:pygame.surface):
+    def draw(self, surface:pygame.surface) -> None:
         'Draws the target to the provided surface'
         
-        surface.blit(self.image, self.coordinates)
+        surface.blit(self.image, self.targetRect)
         
